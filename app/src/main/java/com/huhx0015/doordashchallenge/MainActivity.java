10aeca04ffc9168e937efdf69c3e2fd32d6c442e
huhx0015 <mhuh@yoonhuh.com>
2017-06-01 18:17:52 -0700

    MainActivity: Fixed binding references for appbar and contentmain.

--- app/src/main/java/com/huhx0015/doordashchallenge/MainActivity.java
+++ app/src/main/java/com/huhx0015/doordashchallenge/MainActivity.java
@@ -2,15 +2,10 @@ package com.huhx0015.doordashchallenge;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import com.huhx0015.doordashchallenge.databinding.ActivityMainBinding;
@@ -23,46 +18,23 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private ActivityMainBinding mActivityMainBinding;
     private AppBarMainBinding mAppBarMainBinding;
     private ContentMainBinding mContentMainBinding;
-    private NavHeaderMainBinding mNavHeaderMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         initBinding();
+        initView();
 
 
 
 
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+        if (mActivityMainBinding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mActivityMainBinding.drawerLayout.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
@@ -110,15 +82,35 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        mActivityMainBinding.drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
 
     private void initBinding() {
         mActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        mAppBarMainBinding = AppBarMainBinding.inflate(getLayoutInflater());
-        mContentMainBinding = ContentMainBinding.inflate(getLayoutInflater());
-        mNavHeaderMainBinding = NavHeaderMainBinding.inflate(getLayoutInflater());
+        mAppBarMainBinding = mActivityMainBinding.appBarMain;
+        mContentMainBinding = mAppBarMainBinding.contentMain;
+    }
+
+    private void initView() {
+        initToolbar();
+        initDrawer();
+        initNavigationView();
+    }
+
+    private void initToolbar() {
+        setSupportActionBar(mActivityMainBinding.appBarMain.toolbar);
+    }
+
+    private void initDrawer() {
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mActivityMainBinding.drawerLayout, mAppBarMainBinding.toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mActivityMainBinding.drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    private void initNavigationView() {
+        mActivityMainBinding.navView.setNavigationItemSelectedListener(this);
     }
 }
