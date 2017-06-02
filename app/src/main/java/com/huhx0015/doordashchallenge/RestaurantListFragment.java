42ac9c636d3dc8f798f689d3a601ae0031b7f94f
huhx0015 <mhuh@yoonhuh.com>
2017-06-01 17:51:53 -0700

    Added RestaurantListFragment and basic XML layout, with databinding.

---
+++ app/src/main/java/com/huhx0015/doordashchallenge/RestaurantListFragment.java
@@ -0,0 +1,31 @@
+package com.huhx0015.doordashchallenge;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.huhx0015.doordashchallenge.databinding.FragmentRestaurantListBinding;
+
+/**
+ * Created by Michael Yoon Huh on 6/1/2017.
+ */
+
+public class RestaurantListFragment extends Fragment {
+
+    private FragmentRestaurantListBinding mBinding;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mBinding = DataBindingUtil.inflate(getActivity().getLayoutInflater(), R.layout.fragment_restaurant_list, null, false);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+}
