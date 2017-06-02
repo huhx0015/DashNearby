627c12aa5fec8e81aec1ba9f80b85bcd4d19c4f5
huhx0015 <mhuh@yoonhuh.com>
2017-06-02 00:21:21 -0700

    Added new icons for the navigation drawer. Removed unused default assets.

--- app/src/main/java/com/huhx0015/doordashchallenge/view/activities/MainActivity.java
+++ app/src/main/java/com/huhx0015/doordashchallenge/view/activities/MainActivity.java
@@ -70,20 +70,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_discover) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_favorites) {
 
         }
 
