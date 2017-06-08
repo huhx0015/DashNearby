6746e6301828572abe0d66bf87d233f0b11f8efb
huhx0015 <huhx0015@gmail.com>
2017-06-07 17:49:05 -0700

    NetworkModule: Removed unused interceptor code.

--- app/src/main/java/com/huhx0015/doordashchallenge/injections/modules/NetworkModule.java
+++ app/src/main/java/com/huhx0015/doordashchallenge/injections/modules/NetworkModule.java
@@ -5,17 +5,11 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.huhx0015.doordashchallenge.constants.RestaurantConstants;
-
-import java.io.IOException;
-
 import javax.inject.Singleton;
 import dagger.Module;
 import dagger.Provides;
 import okhttp3.Cache;
-import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -52,24 +46,8 @@ public class NetworkModule {
     OkHttpClient provideOkHttpClient(Cache cache) {
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-
-        //OkHttpClient.Builder httpClient = new OkHttpClient.Builder()
-//        Interceptor authTokenInterceptor = new Interceptor() {
-//            @Override
-//            public Response intercept(Interceptor.Chain chain) throws IOException {
-//                Request original = chain.request();
-//
-//                Request.Builder requestBuilder = original.newBuilder()
-//                        .header("Authorization", "token"); // <-- this is the important line
-//
-//                Request request = requestBuilder.build();
-//                return chain.proceed(request);
-//            }
-//        };
-
         return new OkHttpClient.Builder()
                 .addInterceptor(interceptor)
-                //.addInterceptor(authTokenInterceptor)
                 .cache(cache)
                 .build();
     }
